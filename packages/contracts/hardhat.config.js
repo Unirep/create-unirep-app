c1d25683247af9d3df376e0c29e32d84fcb1873f
Chance <jchancehud@gmail.com>
2022-11-16 21:27:31 -0600

    refactor(contracts): deploy script

--- packages/contracts/hardhat.config.js
+++ packages/contracts/hardhat.config.js
@@ -7,9 +7,11 @@ module.exports = {
             blockGasLimit: 12000000,
         },
         local: {
-            chainId: 34567,
             url: 'http://localhost:8545',
             blockGasLimit: 12000000,
+            accounts: [
+                '0xac0974bec39a17e36ba4a6b4d238ff944bacb478cbed5efcae784d7bf4f2ff80'
+            ]
         },
     },
     solidity: {
