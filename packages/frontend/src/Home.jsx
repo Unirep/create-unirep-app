8c03fde9f3e5d39f86964911684aef0c956d6479
Chance <jchancehud@gmail.com>
2022-12-01 18:06:21 -0600

    feat(frontend): layout fixes/cleanup, logic fixes

--- packages/frontend/src/Home.jsx
+++ packages/frontend/src/Home.jsx
@@ -8,13 +8,9 @@ import User from './contexts/User'
 export default observer(() => {
   const userContext = React.useContext(User)
   const [remainingTime, setRemainingTime] = React.useState(0)
-  const [posRep, setPosRep] = React.useState(0)
-  const [negRep, setNegRep] = React.useState(0)
-  const [graffitiPreimage, setGraffitiPreImage] = React.useState('0')
-  const [nonce, setNonce] = React.useState(0)
-  const [latestGraffitiPreimage, setLatestGraffitiPreimage] = React.useState(0)
-  const [minRep, setMinRep] = React.useState(0)
-  const [proveGraffitiPreimage, setProveGraffitiPreimage] = React.useState(0)
+  const [reqRep, setReqRep] = React.useState({})
+  const [repProofInputs, setRepProofInputs] = React.useState({})
+  const [repProof, setRepProof] = React.useState(null)
 
   const updateTimer = () => {
     if (!userContext.userState) {
@@ -24,94 +20,167 @@ export default observer(() => {
     const time = userContext.userState.calcEpochRemainingTime()
     setRemainingTime(time)
   }
+
   React.useEffect(() => {
     setInterval(() => {
       updateTimer()
     }, 1000)
   }, [])
 
-  return (
-    <div>
-      {
-        !userContext.hasSignedUp ? (
-          <Button onClick={() => userContext.signup()}>Join</Button>
-        ) :
-        <div>
-          <div>Remaining time in epoch: {remainingTime}</div>
-              <div>Current epoch {userContext.currentEpoch}</div>
-              <div>Latest transition epoch {userContext.latestTransitionedEpoch}</div>
-              <div style={{ fontSize: '18px', fontWeight: '500'}}>
-                Total Reputation
-              </div>
-              <div>Pos Rep {userContext.reputation.posRep?.toString()}</div>
-              <div>Neg Rep {userContext.reputation.negRep?.toString()}</div>
-              <div>Graffiti {userContext.reputation.graffiti?.toString()}</div>
-              <div style={{ fontSize: '18px', fontWeight: '500'}}>
-                Provable Reputation
-              </div>
-              <div>Pos Rep {userContext.provableReputation.posRep?.toString()}</div>
-              <div>Neg Rep {userContext.provableReputation.negRep?.toString()}</div>
-              <div>Graffiti {userContext.provableReputation.graffiti?.toString()}</div>
-              <div>
-              <p>pos rep</p>
-              <input
-                value={posRep}
-                onChange={(event) => {
-                  setPosRep(event.target.value ?? 0)
-                } }
-              ></input>
-              <p>current pos rep: {userContext.reputation.posRep.toString()}</p>
-              <p>neg rep</p>
-              <input
-                value={negRep}
-                onChange={(event) => {
-                  setNegRep(event.target.value ?? 0)
-                } }
-              ></input>
-              <p>current neg rep: {userContext.reputation.negRep.toString()}</p>
-              <p>graffiti preimage</p>
-              <input
-                value={graffitiPreimage}
-                onChange={(event) => {
-                  setGraffitiPreImage(event.target.value ?? 0)
-                } }
-              ></input>
-              <p>current graffiti preimage: {latestGraffitiPreimage}</p>
-              <p>epoch key nonce</p>
-              <input
-                value={nonce}
-                onChange={(event) => { setNonce(event.target.value ?? 0) } }
-              ></input>
-              <p>current epoch key nonce: {nonce}</p>
+  React.useEffect(() => {
+    if (!userContext.userState) {
+      setTimeout(() => {
+        userContext.epochKey(reqRep.nonce ?? 0).then((key) => setReqRep((v) => ({ ...v, epochKey: key })))
+      }, 1000)
+    } else {
+        userContext.epochKey(reqRep.nonce ?? 0).then((key) => setReqRep((v) => ({ ...v, epochKey: key })))
+    }
+  }, [reqRep.nonce])
 
-              <Button onClick={()=> {
-                setLatestGraffitiPreimage(graffitiPreimage)
-                return userContext.requestReputation(posRep, negRep, graffitiPreimage, nonce)
-                }}>request</Button>
-              <br/>
-              <Button onClick={()=> userContext.stateTransition()}>transition</Button>
-              <br/>
-              <p>min rep</p>
-              <input
-                value={minRep}
-                onChange={(event) => {
-                  setMinRep(event.target.value ?? 0)
-                } }
-              ></input>
-              <p>graffiti preimage</p>
-              <input
-                value={proveGraffitiPreimage}
-                onChange={(event) => {
-                  setProveGraffitiPreimage(event.target.value ?? 0)
-                } }
-              ></input>
-              <Button onClick={()=> userContext.proveReputation(minRep, proveGraffitiPreimage)}>prove</Button>
-              <div>public signals: {userContext.publicSignals?.map(n=>n.toString())}</div>
-              <div>proof: {userContext.proof?.map(n=>n.toString())}</div>
-              <div>Is proof valid?: {userContext.valid?.toString()}</div>
-            </div>
+  if (!userContext.userState) {
+    return (
+      <div className="container">
+        Loading...
+      </div>
+    )
+  }
+
+  if (!userContext.hasSignedUp) {
+    return (
+      <div className="container">
+        <Button onClick={() => userContext.signup()}>Join</Button>
+      </div>
+    )
+  }
+
+  return (
+    <div className="container">
+      <div className="info-container">
+        <div>Remaining time in epoch: {remainingTime}</div>
+        <div>Current epoch: {userContext.userState?.calcCurrentEpoch()}</div>
+        <div>Latest transition epoch: {userContext.latestTransitionedEpoch}</div>
+      </div>
+      <div className="info-container">
+        <div className="title1">
+          Total Reputation
         </div>
-      }
+        <div>Pos Rep: {userContext.reputation.posRep?.toString()}</div>
+        <div>Neg Rep: {userContext.reputation.negRep?.toString()}</div>
+        <div>Graffiti: 0x{userContext.reputation.graffiti?.toString(16)}</div>
+      </div>
+      <div className="info-container">
+        <div className="title1">
+          Provable Reputation
+        </div>
+        <div>Pos Rep: {userContext.provableReputation.posRep?.toString()}</div>
+        <div>Neg Rep: {userContext.provableReputation.negRep?.toString()}</div>
+        <div>Graffiti: 0x{userContext.provableReputation.graffiti?.toString(16)}</div>
+      </div>
+      <div style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap' }}>
+        <div className="action-container">
+          <div className="title1">Request reputation</div>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <p>Positive reputation:</p>
+            <div style={{ width: '4px' }} />
+            <input
+              value={reqRep.posRep ?? ''}
+              onChange={(event) => {
+                if (!/^\d*$/.test(event.target.value)) return
+                setReqRep((v) => ({...v, posRep: event.target.value ?? 0}))
+              } }
+            />
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <p>Negative reputation:</p>
+            <div style={{ width: '4px' }} />
+            <input
+              value={reqRep.negRep ?? ''}
+              onChange={(event) => {
+                  if (!/^\d*$/.test(event.target.value)) return
+                  setReqRep((v) => ({...v, negRep: event.target.value ?? 0}))
+              } }
+            />
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <p>Graffiti preimage:</p>
+            <div style={{ width: '4px' }} />
+            <input
+              value={reqRep.graffitiPreImage ?? ''}
+              onChange={(event) => {
+                setReqRep((v) => ({ ...v, graffitiPreImage: event.target.value ?? 0 }))
+              } }
+            />
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <p>Epoch key nonce</p>
+            <div style={{ width: '4px' }} />
+            <select value={reqRep.nonce ?? 0} onChange={(event) => {
+              setReqRep((v) => ({ ...v, nonce: event.target.value }))
+            }}>
+              <option value="0">0</option>
+              <option value="1">1</option>
+              <option value="2">2</option>
+            </select>
+          </div>
+          <p>Requesting reputation with epoch key: {reqRep.epochKey ?? ''}</p>
+
+          <Button onClick={async () => {
+            await userContext.requestReputation(
+              reqRep.posRep ?? 0,
+              reqRep.negRep ?? 0,
+              reqRep.graffitiPreimage ?? 0,
+              reqRep.nonce ?? 0
+            )
+            setReqRep({})
+          }}>
+            Request Rep
+          </Button>
+        </div>
+        <div className="action-container">
+          <div className="title1">Prove reputation</div>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <p>Minimum reputation:</p>
+            <div style={{ width: '4px' }} />
+            <input
+              value={repProofInputs.minRep ?? ''}
+              onChange={(event) => {
+                if (!/^\d*$/.test(event.target.value)) return
+                setRepProofInputs((v) => ({ ...v, minRep: event.target.value ?? 0}))
+              }}
+            />
+          </div>
+          <div style={{ display: 'flex', alignItems: 'center' }}>
+            <p>Graffiti preimage:</p>
+            <div style={{ width: '4px' }} />
+            <input
+              value={repProofInputs.graffitiPreImage ?? ''}
+              onChange={(event) => {
+                setRepProofInputs((v) => ({ ...v, graffitiPreImage: event.target.value ?? 0 }))
+              }}
+            />
+          </div>
+          <Button onClick={async ()=> {
+            const proof = await userContext.proveReputation(repProofInputs.minRep, repProofInputs.graffitiPreimage)
+            setRepProofInputs({})
+            setRepProof(proof)
+          }}>
+            Generate proof
+          </Button>
+          {repProof ?
+            (
+              <>
+                <div>Is proof valid?: {repProof?.valid?.toString() ?? ''}</div>
+                <textarea readOnly value={JSON.stringify(repProof, null, 2)} />
+              </>
+            ) :
+            null
+          }
+        </div>
+      </div>
+      <div className="action-container">
+        <div className="title1">User state transition</div>
+        <Button onClick={()=> userContext.stateTransition()}>Transition</Button>
+      </div>
     </div>
   )
 })
