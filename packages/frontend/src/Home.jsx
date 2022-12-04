f4f63f9e8bd15a1f280d6d33fee6439d2d90df3a
Colleen Rose <cjrose59@gmail.com>
2022-12-04 13:42:28 -0700

    add icons for mouseover info

--- packages/frontend/src/Home.jsx
+++ packages/frontend/src/Home.jsx
@@ -5,6 +5,12 @@ import Button from './components/Button'
 
 import User from './contexts/User'
 
+import { MdOutlineTimelapse} from 'react-icons/md'
+import { BsPersonCircle } from 'react-icons/bs'
+import { HiShieldCheck } from 'react-icons/hi'
+import { HiQuestionMarkCircle } from 'react-icons/hi'
+import { MdPublishedWithChanges } from 'react-icons/md'
+
 export default observer(() => {
   const userContext = React.useContext(User)
   const [remainingTime, setRemainingTime] = React.useState(0)
@@ -76,130 +82,176 @@ export default observer(() => {
   return (
     <div className="container">
       <div className="info-container">
-        <div>Remaining time in epoch: {remainingTime}</div>
-        <div>Current epoch: {userContext.userState?.calcCurrentEpoch()}</div>
-        <div>Latest transition epoch: {userContext.latestTransitionedEpoch}</div>
+        <div class='icon'>
+          <MdOutlineTimelapse color='YellowGreen' font-size='3.5em' />
+        </div>
+        <div>
+          <div className="title1">
+            Epoch
+          </div>
+          <div>Current epoch: {userContext.userState?.calcCurrentEpoch()}</div>
+          <div>Remaining time in epoch: {remainingTime}</div>
+          <div>Latest transition epoch: {userContext.latestTransitionedEpoch}</div>
+        </div>
+        
       </div>
       <div className="info-container">
-        <div className="title1">
-          Total Reputation
+        <div class='icon'>
+          <BsPersonCircle color='YellowGreen' font-size='3em' />
         </div>
-        <div>Pos Rep: {userContext.reputation.posRep?.toString()}</div>
-        <div>Neg Rep: {userContext.reputation.negRep?.toString()}</div>
-        <div>Graffiti: 0x{userContext.reputation.graffiti?.toString(16)}</div>
+        <div>
+          <div className="title1">
+            Total Reputation
+          </div>
+          <div>Pos Rep: {userContext.reputation.posRep?.toString()}</div>
+          <div>Neg Rep: {userContext.reputation.negRep?.toString()}</div>
+          <div>Graffiti: 0x{userContext.reputation.graffiti?.toString(16)}</div>
+        </div>
       </div>
       <div className="info-container">
-        <div className="title1">
-          Provable Reputation
+        <div class='icon'>
+          <HiShieldCheck color='YellowGreen' font-size='3.5em' />
         </div>
-        <div>Pos Rep: {userContext.provableReputation.posRep?.toString()}</div>
-        <div>Neg Rep: {userContext.provableReputation.negRep?.toString()}</div>
-        <div>Graffiti: 0x{userContext.provableReputation.graffiti?.toString(16)}</div>
+        <div>
+          <div className="title1">
+            Provable Reputation
+          </div>
+          <div>Pos Rep: {userContext.provableReputation.posRep?.toString()}</div>
+          <div>Neg Rep: {userContext.provableReputation.negRep?.toString()}</div>
+          <div>Graffiti: 0x{userContext.provableReputation.graffiti?.toString(16)}</div>
+        </div>
       </div>
       <div style={{ display: 'flex', justifyContent: 'space-around', flexWrap: 'wrap' }}>
+      
         <div className="action-container">
-          <div className="title1">Request reputation</div>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <p>Positive reputation:</p>
-            <div style={{ width: '4px' }} />
-            <input
-              value={reqRep.posRep ?? ''}
-              onChange={(event) => {
-                if (!/^\d*$/.test(event.target.value)) return
-                setReqRep((v) => ({...v, posRep: event.target.value ?? 0}))
-              } }
-            />
-          </div>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <p>Negative reputation:</p>
-            <div style={{ width: '4px' }} />
-            <input
-              value={reqRep.negRep ?? ''}
-              onChange={(event) => {
+          <div>
+            <div className="title2">
+              <div class='icon-sm'>
+                <HiQuestionMarkCircle color='yellowgreen' font-size='.8em'/>
+              </div>
+              <div>Request Reputation</div>
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <p>Positive reputation:</p>
+              <div style={{ width: '4px' }} />
+              <input
+                value={reqRep.posRep ?? ''}
+                onChange={(event) => {
                   if (!/^\d*$/.test(event.target.value)) return
-                  setReqRep((v) => ({...v, negRep: event.target.value ?? 0}))
-              } }
-            />
-          </div>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <p>Graffiti preimage:</p>
-            <div style={{ width: '4px' }} />
-            <input
-              value={reqRep.graffitiPreImage ?? ''}
-              onChange={(event) => {
-                setReqRep((v) => ({ ...v, graffitiPreImage: event.target.value ?? 0 }))
-              } }
-            />
-          </div>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <p>Epoch key nonce</p>
-            <div style={{ width: '4px' }} />
-            <select value={reqRep.nonce ?? 0} onChange={(event) => {
-              setReqRep((v) => ({ ...v, nonce: event.target.value }))
-            }}>
-              <option value="0">0</option>
-              <option value="1">1</option>
-              <option value="2">2</option>
-            </select>
-          </div>
-          <p>Requesting reputation with epoch key: {reqRep.epochKey ?? ''}</p>
+                  setReqRep((v) => ({...v, posRep: event.target.value ?? 0}))
+                } }
+              />
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <p>Negative reputation:</p>
+              <div style={{ width: '4px' }} />
+              <input
+                value={reqRep.negRep ?? ''}
+                onChange={(event) => {
+                    if (!/^\d*$/.test(event.target.value)) return
+                    setReqRep((v) => ({...v, negRep: event.target.value ?? 0}))
+                } }
+              />
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <div class='icon-sm'>
+                <HiQuestionMarkCircle color='yellowgreen' font-size='1em'/>
+              </div>
+              <p>Graffiti preimage:</p>
+              <div style={{ width: '4px' }} />
+              <input
+                value={reqRep.graffitiPreImage ?? ''}
+                onChange={(event) => {
+                  setReqRep((v) => ({ ...v, graffitiPreImage: event.target.value ?? 0 }))
+                } }
+              />
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <div class='icon-sm'>
+                <HiQuestionMarkCircle color='yellowgreen' font-size='1em'/>
+              </div>
+              <p>Epoch key nonce</p>
+              <div style={{ width: '4px' }} />
+              <select value={reqRep.nonce ?? 0} onChange={(event) => {
+                setReqRep((v) => ({ ...v, nonce: event.target.value }))
+              }}>
+                <option value="0">0</option>
+                <option value="1">1</option>
+                <option value="2">2</option>
+              </select>
+            </div>
+            <p>Requesting reputation with epoch key: {reqRep.epochKey ?? ''}</p>
 
-          <Button onClick={async () => {
-            await userContext.requestReputation(
-              reqRep.posRep ?? 0,
-              reqRep.negRep ?? 0,
-              reqRep.graffitiPreimage ?? 0,
-              reqRep.nonce ?? 0
-            )
-            setReqRep({})
-          }}>
-            Request Rep
-          </Button>
+            <Button onClick={async () => {
+              await userContext.requestReputation(
+                reqRep.posRep ?? 0,
+                reqRep.negRep ?? 0,
+                reqRep.graffitiPreimage ?? 0,
+                reqRep.nonce ?? 0
+              )
+              setReqRep({})
+            }}>
+              Request Rep
+            </Button>
+          </div>
         </div>
         <div className="action-container">
-          <div className="title1">Prove reputation</div>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <p>Minimum reputation:</p>
-            <div style={{ width: '4px' }} />
-            <input
-              value={repProofInputs.minRep ?? ''}
-              onChange={(event) => {
-                if (!/^\d*$/.test(event.target.value)) return
-                setRepProofInputs((v) => ({ ...v, minRep: event.target.value ?? 0}))
-              }}
-            />
+          <div>
+            <div className="title2">
+              <div class='icon-sm'>
+                <HiQuestionMarkCircle color='yellowgreen' font-size='.8em'/>
+              </div>
+              <div>Prove Reputation</div>
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <p>Minimum reputation:</p>
+              <div style={{ width: '4px' }} />
+              <input
+                value={repProofInputs.minRep ?? ''}
+                onChange={(event) => {
+                  if (!/^\d*$/.test(event.target.value)) return
+                  setRepProofInputs((v) => ({ ...v, minRep: event.target.value ?? 0}))
+                }}
+              />
+            </div>
+            <div style={{ display: 'flex', alignItems: 'center' }}>
+              <p>Graffiti preimage:</p>
+              <div style={{ width: '4px' }} />
+              <input
+                value={repProofInputs.graffitiPreImage ?? ''}
+                onChange={(event) => {
+                  setRepProofInputs((v) => ({ ...v, graffitiPreImage: event.target.value ?? 0 }))
+                }}
+              />
+            </div>  
+            <Button onClick={async ()=> {
+              const proof = await userContext.proveReputation(repProofInputs.minRep, repProofInputs.graffitiPreimage)
+              setRepProofInputs({})
+              setRepProof(proof)
+            }}>
+              Generate Proof
+            </Button>
+            {repProof ?
+              (
+                <>
+                  <div>Is proof valid?: {repProof?.valid?.toString() ?? ''}</div>
+                  <textarea readOnly value={JSON.stringify(repProof, null, 2)} />
+                </>
+              ) :
+              null
+            }
           </div>
-          <div style={{ display: 'flex', alignItems: 'center' }}>
-            <p>Graffiti preimage:</p>
-            <div style={{ width: '4px' }} />
-            <input
-              value={repProofInputs.graffitiPreImage ?? ''}
-              onChange={(event) => {
-                setRepProofInputs((v) => ({ ...v, graffitiPreImage: event.target.value ?? 0 }))
-              }}
-            />
-          </div>
-          <Button onClick={async ()=> {
-            const proof = await userContext.proveReputation(repProofInputs.minRep, repProofInputs.graffitiPreimage)
-            setRepProofInputs({})
-            setRepProof(proof)
-          }}>
-            Generate proof
-          </Button>
-          {repProof ?
-            (
-              <>
-                <div>Is proof valid?: {repProof?.valid?.toString() ?? ''}</div>
-                <textarea readOnly value={JSON.stringify(repProof, null, 2)} />
-              </>
-            ) :
-            null
-          }
         </div>
       </div>
-      <div className="action-container">
-        <div className="title1">User state transition</div>
-        <Button onClick={()=> userContext.stateTransition()}>Transition</Button>
+
+      <div className="action-container transition">
+        <div class='icon'>
+          <MdPublishedWithChanges color='YellowGreen' font-size='4em' />
+        </div>
+        <div>
+          <div className="title2">User state transition</div>
+          <Button onClick={()=> userContext.stateTransition()}>Transition</Button>
+        </div>
       </div>
     </div>
   )
