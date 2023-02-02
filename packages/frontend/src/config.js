228ff0e48c000f0d799177d4abc70cf35d450180
Chance <jchancehud@gmail.com>
2023-02-01 20:25:21 -0600

    deploy settings

--- packages/frontend/src/config.js
+++ packages/frontend/src/config.js
@@ -6,4 +6,5 @@ export const ETH_PROVIDER_URL = config.ETH_PROVIDER_URL
 
 export const provider = ETH_PROVIDER_URL.startsWith('http') ? new ethers.providers.JsonRpcProvider(ETH_PROVIDER_URL) : new ethers.providers.WebSocketProvider(ETH_PROVIDER_URL)
 
-export const SERVER = 'http://localhost:8000'
+// export const SERVER = 'http://localhost:8000'
+export const SERVER = 'https://relay.demo.unirep.io'
