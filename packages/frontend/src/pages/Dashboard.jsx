b281b51de9d366b311b6bd440e308b96aac17476
Chance <jchancehud@gmail.com>
2023-04-11 19:52:56 -0500

    feat(frontend): parse the replacement field data

--- packages/frontend/src/pages/Dashboard.jsx
+++ packages/frontend/src/pages/Dashboard.jsx
@@ -74,7 +74,17 @@ export default observer(() => {
               return (
                 <div key={i} className='info-item'>
                   <div>Data {i}</div>
-                  <div className='stat'>{(data || 0).toString()}</div>
+                  {
+                    i >= userContext.userState?.sync.settings.sumFieldCount ? (
+                      <div className='stat'>
+                        <span>{(BigInt(data ?? 0) >> BigInt(254-userContext.userState?.sync.settings.replNonceBits)).toString()}</span>
+                        <span>:</span>
+                        <span>{(BigInt(data ?? 0) & (2n**BigInt(254-userContext.userState?.sync.settings.replNonceBits) - 1n)).toString()}</span>
+                      </div>
+                      ) : (
+                      <div className='stat'>{(data || 0).toString()}</div>
+                      )
+                  }
                 </div>
               )
             })
