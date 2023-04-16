c802fd4dc94bd8de504c1a2bc124e728cf833b0c
Chance <jchancehud@gmail.com>
2023-04-15 23:16:22 -0500

    fix: update keys/dockerfile

--- Dockerfile
+++ Dockerfile
@@ -8,8 +8,6 @@ RUN yarn && rm -rf packages/frontend
 
 RUN sh scripts/loadKeys.sh
 
-RUN rm -r packages/relay/keys/buildOrdered*
-
 FROM node:16-buster
 
 COPY --from=0 /src /src
--- scripts/loadKeys.sh
+++ scripts/loadKeys.sh
@@ -5,9 +5,9 @@ set -e
 WORKDIR=$(mktemp)
 TARFILE=$WORKDIR/keys.tar.gz
 
-wget https://keys.unirep.io/2-beta-1/keys.tar.gz -P $WORKDIR --progress=bar:force:noscroll
+wget https://keys.unirep.io/2-beta-2/keys.tar.gz -P $WORKDIR --progress=bar:force:noscroll
 
-shasum -a 256 $TARFILE | grep '99891da0525f617ba382e18e2cc6c9b55d45b55711dd789403d31b3945e44e34'
+shasum -a 256 $TARFILE | grep '008e18873485ab3d81194fe17819d2b7e0d3bc40d9c5374c63e6450ce87efa25'
 
 tar -xzf $TARFILE -C packages/relay
 mv packages/relay/zksnarkBuild packages/relay/keys
