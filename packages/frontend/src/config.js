2a05b389881a31fb8207d3543107f62a619f9855
Chance <jchancehud@gmail.com>
2023-04-17 21:27:07 -0500

    chore: beta-3 support

--- packages/frontend/src/config.js
+++ packages/frontend/src/config.js
@@ -9,4 +9,4 @@ export const provider = ETH_PROVIDER_URL.startsWith('http') ? new ethers.provide
 export const SERVER = 'http://localhost:8000'
 // export const SERVER = 'https://relay.demo.unirep.io'
 export const KEY_SERVER = 'http://localhost:8000/build/'
-// export const KEY_SERVER = 'https://keys.unirep.io/2-beta-1/'
+export const KEY_SERVER = 'https://keys.unirep.io/2-beta-3/'
