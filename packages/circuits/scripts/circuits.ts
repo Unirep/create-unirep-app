90cc209b8bd5de4b9a1bed816d40e3e27d2b6b91
s-ekai <szkmsyk041792@gmail.com>
2023-09-07 17:13:53 +0900

    use: pragma circom 2.1.0 in scripts/circuits.ts

--- packages/circuits/scripts/circuits.ts
+++ packages/circuits/scripts/circuits.ts
@@ -4,5 +4,5 @@ const { STATE_TREE_DEPTH, FIELD_COUNT, SUM_FIELD_COUNT } = CircuitConfig.default
 export const ptauName = 'powersOfTau28_hez_final_18.ptau'
 
 export const circuitContents = {
-    dataProof: `pragma circom 2.0.0; include "../circuits/dataProof.circom"; \n\ncomponent main { public [ value ] } = DataProof(${STATE_TREE_DEPTH}, ${FIELD_COUNT}, ${SUM_FIELD_COUNT});`,
+    dataProof: `pragma circom 2.1.0; include "../circuits/dataProof.circom"; \n\ncomponent main { public [ value ] } = DataProof(${STATE_TREE_DEPTH}, ${FIELD_COUNT}, ${SUM_FIELD_COUNT});`,
 }
